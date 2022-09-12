75f13fa7cd4f5454701903ca81611929e9dd3de0
Mightybeardedking <35457893+Mightybeardedking@users.noreply.github.com>
2022-09-12 14:44:31 +0200

    Delete Office.ps1

--- Office.ps1
+++
@@ -1,6 +0,0 @@
-$OfficeUninstallStrings = (Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Where {$_.DisplayName -like "*Microsoft Office 365*"} | Select UninstallString).UninstallString
-    ForEach ($UninstallString in $OfficeUninstallStrings) {
-        $UninstallEXE = ($UninstallString -split '"')[1]
-        $UninstallArg = ($UninstallString -split '"')[2] + " DisplayLevel=False"
-        Start-Process -FilePath $UninstallEXE -ArgumentList $UninstallArg -Wait
-    }    
