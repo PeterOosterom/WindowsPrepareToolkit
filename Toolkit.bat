7c4dba7eb2829a88e47931b301430b908155c329
Mightybeardedking <35457893+Mightybeardedking@users.noreply.github.com>
2022-09-19 15:48:52 +0200

    Update Toolkit.bat

--- Toolkit.bat
+++ Toolkit.bat
@@ -1,3 +1,3 @@
 @echo off
-Powershell.exe -Command Set-ExecutionPolicy Unrestricted -Scope Process; Powershell.exe -File ".\Office.ps1"
-
+powershell.exe -Command "Start-Process -Verb RunAs powershell -ArgumentList '-NoProfile -NoExit -ExecutionPolicy Bypass -File "C:\Office.ps1\"'
+pause
