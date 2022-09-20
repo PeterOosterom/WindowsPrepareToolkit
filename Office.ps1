24e6922ff499939eae48918398626bf7d7a5f55b
Mightybeardedking <35457893+Mightybeardedking@users.noreply.github.com>
2022-09-20 15:40:01 +0200

    Update Office.ps1

--- Office.ps1
+++ Office.ps1
@@ -10,6 +10,7 @@ Get-AppxProvisionedPackage -online | %{if ($_.packagename -match "Microsoft.Zune
 Get-AppxProvisionedPackage -online | %{if ($_.packagename -match "Microsoft.MicrosoftOffice") {$_ | Remove-AppxProvisionedPackage -AllUsers}}
 Get-AppxProvisionedPackage -online | %{if ($_.packagename -match "LinkedInforWindows") {$_ | Remove-AppxProvisionedPackage -AllUsers}}
 Get-AppxProvisionedPackage -online | %{if ($_.packagename -match "Microsoft.LinkedInforWindows") {$_ | Remove-AppxProvisionedPackage -AllUsers}}
+Get-AppxPackage | %{if ($_.name -match "MicrosoftTeams") {$_ | Remove-AppxPackage -AllUsers}}
 # Gets the list of 3rd party bloatware
 get-appxpackage | select name
 # Remove 3rd party bloatware
@@ -18,6 +19,7 @@ Get-AppxPackage | %{if ($_.name -match "king.com") {$_ | Remove-AppxPackage -All
 Get-AppxPackage | %{if ($_.name -match "Microsoft.zune") {$_ | Remove-AppxPackage -AllUsers}}
 Get-AppxPackage | %{if ($_.name -match "microsoft.skypeapp") {$_ | Remove-AppxPackage -AllUsers}}
 Get-AppxPackage | %{if ($_.name -match "NORDCURRENT.COOKINGFEVER") {$_ | Remove-AppxPackage -AllUsers}}
+Get-AppxPackage | %{if ($_.name -match "Microsoft.MicrosoftSolitaireCollection") {$_ | Remove-AppxPackage -AllUsers}}
 # Unpin Microsoft bs from start menu
 
 (New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items()| foreach { ($_).Verbs() | ?{$_.Name.Replace('&', '') -match 'From "Start" UnPin|Unpin from Start'} | %{$_.DoIt()} }
