284d0eee1d2c48392db6a97e51be6487b829a265
Mightybeardedking <35457893+Mightybeardedking@users.noreply.github.com>
2022-09-20 15:25:57 +0200

    Update Office.ps1

--- Office.ps1
+++ Office.ps1
@@ -1,7 +1,7 @@
 
-# Get the list of Provisioned packages
+# Get the list of Microsoft bloatware
 Get-AppxProvisionedPackage -online | select packagename
-# Remove packages that start with the Match string for All Users
+# Remove Microsoft bloatware
 Get-AppxProvisionedPackage -online | %{if ($_.packagename -match "Microsoft.Office.Desktop") {$_ | Remove-AppxProvisionedPackage -AllUsers}}
 Get-AppxProvisionedPackage -online | %{if ($_.packagename -match "Microsoft.Office.OneNote") {$_ | Remove-AppxProvisionedPackage -AllUsers}}
 Get-AppxProvisionedPackage -online | %{if ($_.packagename -match "Microsoft.Xbox") {$_ | Remove-AppxProvisionedPackage -AllUsers}}
@@ -10,15 +10,15 @@ Get-AppxProvisionedPackage -online | %{if ($_.packagename -match "Microsoft.Zune
 Get-AppxProvisionedPackage -online | %{if ($_.packagename -match "Microsoft.MicrosoftOffice") {$_ | Remove-AppxProvisionedPackage -AllUsers}}
 Get-AppxProvisionedPackage -online | %{if ($_.packagename -match "LinkedInforWindows") {$_ | Remove-AppxProvisionedPackage -AllUsers}}
 Get-AppxProvisionedPackage -online | %{if ($_.packagename -match "Microsoft.LinkedInforWindows") {$_ | Remove-AppxProvisionedPackage -AllUsers}}
-# Get the list of other packages
+# Gets the list of 3rd party bloatware
 get-appxpackage | select name
-# Remove packages that start with the Match string for All Users
+# Remove 3rd party bloatware
 Get-AppxPackage | %{if ($_.name -match "Fitbit.") {$_ | Remove-AppxPackage -AllUsers}}
 Get-AppxPackage | %{if ($_.name -match "king.com") {$_ | Remove-AppxPackage -AllUsers}}
 Get-AppxPackage | %{if ($_.name -match "Microsoft.zune") {$_ | Remove-AppxPackage -AllUsers}}
 Get-AppxPackage | %{if ($_.name -match "microsoft.skypeapp") {$_ | Remove-AppxPackage -AllUsers}}
 Get-AppxPackage | %{if ($_.name -match "NORDCURRENT.COOKINGFEVER") {$_ | Remove-AppxPackage -AllUsers}}
-# Unpin EVERYTHING (well, at least almost everything) from the start menu (clean slate)
+# Unpin Microsoft bs from start menu
 
 (New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items()| foreach { ($_).Verbs() | ?{$_.Name.Replace('&', '') -match 'From "Start" UnPin|Unpin from Start'} | %{$_.DoIt()} }
 
