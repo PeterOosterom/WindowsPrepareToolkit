b4f152c0e568089e386052eb847f73073b643627
Mightybeardedking <35457893+Mightybeardedking@users.noreply.github.com>
2022-09-20 15:24:27 +0200

    Update Toolkit.bat

--- Toolkit.bat
+++ Toolkit.bat
@@ -1,3 +1,4 @@
+#Starts a priveleged ps shell and changes the execution policy
 @echo off
-powershell.exe -Command "Start-Process -Verb RunAs powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File "C:\Office.ps1\"'
+powershell.exe -Command "Start-Process -Verb RunAs powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File "C:\Caller.ps1\"'
 
