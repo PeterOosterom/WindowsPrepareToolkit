eda1ab562289d65c8887154f1a158d03654e365c
PeterOosterom <35457893+PeterOosterom@users.noreply.github.com>
2022-09-21 08:04:24 +0200

    Update Caller.ps1

--- Caller.ps1
+++ Caller.ps1
@@ -1,7 +1,8 @@
-#Calls the scripts one after the other 
+#Calls the scripts one after the other and restarts the pc afterwards
 C:\Office.ps1
 Start-Sleep -Seconds 5
 C:\Software.ps1
 Start-Sleep -Seconds 5
 C:\start.ps1
-
+Start-Sleep -Seconds 5
+Restart-Computer
