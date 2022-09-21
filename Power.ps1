892baac1a26d9e99d08db2e838600451d6d9055d
PeterOosterom <35457893+PeterOosterom@users.noreply.github.com>
2022-09-21 08:32:55 +0200

    Update Power.ps1

--- Power.ps1
+++ Power.ps1
@@ -1,5 +1,5 @@
-#Changes the power settings to turn off monitor/device after 5 hours
+#Changes the power settings to turn off monitor after 5 hours
 powercfg -change -monitor-timeout-dc 300
-powercfg -change -monitor-timeout-ac 0
-powercfg -change -standby-timeout-dc 300
+powercfg -change -monitor-timeout-ac 300
+powercfg -change -standby-timeout-dc 0
 powercfg -change -standby-timeout-ac 0
