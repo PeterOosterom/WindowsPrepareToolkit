77f12595dfb5510673177e0ecb2ec2841cb37a7d
PeterOosterom <35457893+PeterOosterom@users.noreply.github.com>
2022-09-21 08:31:30 +0200

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -1,2 +1,2 @@
 #copies the downloaded start.bin to the correct folder
-Copy-Item "c:\start.bin" "$($env:LOCALAPPDATA)\Packages\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\LocalState\start.bin" -Recurse -force
+Copy-Item "c:\start.bin" "$($env:LOCALAPPDATA)\Packages\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\LocalState\start.bin" -force
