db2a398bfee78404aa709af2ae7d16ee46b17a7d
Peter Oosterom <35457893+PeterOosterom@users.noreply.github.com>
2022-09-26 12:34:19 +0200

    Delete start.ps1

--- start.ps1
+++
@@ -1,2 +0,0 @@
-#copies the downloaded start.bin to the correct folder
-Copy-Item "c:\start.bin" "$($env:LOCALAPPDATA)\Packages\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\LocalState\start.bin" -force
