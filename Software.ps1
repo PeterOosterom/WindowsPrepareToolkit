3191f69b2e489a79f0381cb6b9ecfb168b2c1621
Peter Oosterom <35457893+PeterOosterom@users.noreply.github.com>
2022-09-26 12:33:59 +0200

    Delete Software.ps1

--- Software.ps1
+++
@@ -1,7 +0,0 @@
-#Install Chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-#Install software
-choco install googlechrome -fy
-choco install firefox -fy
-choco install winrar -fy
-choco install adobereader -fy
